3c7a08eb210aad7bebf76b401789569fda520d54
PaiZuZe <vitinho_cga@hotmail.com>
2018-03-06 20:44:39 -0300

    Removes first PLA implemantation and adds method to show current insample error

--- FirstAttempt/mainV2.py
+++ FirstAttempt/mainV2.py
@@ -18,11 +18,15 @@ class learning() :
     def linear_regression(self) :
         self.W = np.dot(np.linalg.pinv(self.input), self.expected)
 
+    def err_insample(self) :
+        count = 0
+        for i in range(len(input)) :
+            result = np.sign(np.dot(self.W, self.input[i]))
+            if result == self.expected[i] :
+                count += 1
+        return count/len(input)
+
 input = [[1, 0, 0], [1, 0, 1], [1, 1 ,0], [1, 1, 1]]
 output = [-1, 1, 1, 1]
 
 example = learning(np.array(input), np.array(output))
-example.PLA(5)
-print(example.W)
-example.linear_regression()
-print(example.W)
