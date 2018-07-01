98031412d8835afad0fde318b5c57a613bfb4fc8
PaiZuZe <vitinho_cga@hotmail.com>
2018-07-01 11:21:55 -0300

    Finishes the cnn.

--- FourthAssigment/cnn.py
+++ FourthAssigment/cnn.py
@@ -19,7 +19,42 @@ class CNN(nn.Module):
                  config):
         super(CNN, self).__init__()
         # YOUR CODE HERE:
-        raise NotImplementedError("falta completar o método __init__ da classe CNN")
+        import math
+
+        self.conv = torch.nn.Sequential()
+
+        self.conv.add_module("conv_0", torch.nn.Conv2d(config.channels, config.conv_architecture[0], config.kernel_sizes[0]))
+
+        new_height = config.height - config.kernel_sizes[0]
+        new_width = config.width - config.kernel_sizes[0]
+
+        self.conv.add_module("relu_0", torch.nn.ReLU())
+        self.conv.add_module("maxpool_0", torch.nn.MaxPool2d(config.pool_kernel[0]))
+
+        new_height = math.ceil(new_height / config.pool_kernel[0])
+        new_width = math.ceil(new_width / config.pool_kernel[0])
+
+        for i in range(1, len(config.conv_architecture)) :
+            self.conv.add_module("conv_" + str(i), torch.nn.Conv2d(config.conv_architecture[i - 1], config.conv_architecture[i], config.kernel_sizes[i]))
+
+            new_height -= config.kernel_sizes[i]
+            new_width -= config.kernel_sizes[i]
+
+            self.conv.add_module("relu_" + str(i), torch.nn.ReLU())
+            self.conv.add_module("maxpool_" + str(i), torch.nn.MaxPool2d(config.pool_kernel[i]))
+
+            new_height = math.ceil(new_height / config.pool_kernel[i])
+            new_width = math.ceil(new_width / config.pool_kernel[i])
+
+
+        self.fc = torch.nn.Sequential()
+
+        self.fc.add_module("lin_0", nn.Linear(config.conv_architecture[-1] * new_height * new_width, config.architecture[0]))
+        self.fc.add_module("ReLu_0", torch.nn.ReLU())
+
+        for i in range(len(config.architecture) - 1) :
+            self.fc.add_module("lin_" + str(i + 1), nn.Linear(config.architecture[i], config.architecture[i + 1]))
+            self.fc.add_module("ReLu_" + str(i + 1), torch.nn.ReLU())
         # END YOUR CODE
 
     def forward(self, x):
@@ -32,7 +67,9 @@ class CNN(nn.Module):
         :rtype: torch.FloatTensor(shape=[batch_size, number_of_classes])
         """
         # YOUR CODE HERE:
-        raise NotImplementedError("falta completar o método forward da classe CNN")
+        logits = self.conv(x)
+        logits = logits.view(logits.size()[0], -1)
+        logits = self.fc(logits)
         # END YOUR CODE
         return logits
 
@@ -46,7 +83,7 @@ class CNN(nn.Module):
         :rtype: torch.LongTensor(shape=[batch_size])
         """
         # YOUR CODE HERE:
-        raise NotImplementedError("falta completar o método predict da classe CNN")
+        predictions =  torch.argmax(nn.Softmax(dim=0)(self.forward(x)), dim=1)
         # END YOUR CODE
         return predictions
 
@@ -75,11 +112,11 @@ def train_model_img_classification(model,
 
     best_valid_loss = float("inf")
     # YOUR CODE HERE:
-    # i) define the loss criteria and the optimizer. 
+    # i) define the loss criteria and the optimizer.
     # You may find nn.CrossEntropyLoss and torch.optim.SGD useful here.
-    raise NotImplementedError("falta completar a função train_model_img_classification")
-    # criterion =  
-    # optimizer =
+    valid = iter(valid_loader)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=config.learning_rate, momentum=config.momentum)
     # END YOUR CODE
 
     train_loss = []
@@ -89,14 +126,16 @@ def train_model_img_classification(model,
             # YOUR CODE HERE:
             # ii) You should zero the model gradients
             # and define the loss function for the train data.
-            raise NotImplementedError("falta completar a função train_model_img_classification")
-            # loss = 
+            images = images / 255
+            optimizer.zero_grad()
+            loss = criterion((model.forward(images)), labels)
             # END YOUR CODE
             if step % config.save_step == 0:
                 # YOUR CODE HERE:
                 # iii) You should define the loss function for the valid data.
-                raise NotImplementedError("falta completar a função train_model_img_classification")
-                # v_loss = 
+                batch_vx, batch_vy = next(valid)
+                batch_vx = batch_vx / 255
+                v_loss = criterion((model.forward(batch_vx)), batch_vy)
                 # END YOUR CODE
                 valid_loss.append(float(v_loss))
                 train_loss.append(float(loss))
@@ -109,7 +148,8 @@ def train_model_img_classification(model,
             # YOUR CODE HERE:
             # iv) You should do the back propagation
             # and do the optimization step.
-            raise NotImplementedError("falta completar a função train_model_img_classification")            
+            loss.backward()
+            optimizer.step()
             # END YOUR CODE
     if verbose:
         x = np.arange(1, len(train_loss) + 1, 1)
